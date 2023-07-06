3b5c861926b5a95aad04d1300271e3ae5fb4845c
minababaee <minababaee8002@gmail.com>
2023-07-06 10:59:51 -0700

    commit2

--- textconvertor.py
+++ textconvertor.py
@@ -7,6 +7,10 @@ main.minsize(400,400)
 main.maxsize(400,400)
 
 def widgets():
-    pass  
+    lbl_start = Label(main, text="speak")
+    lbl_start.config(font=("esthetique",22,"bold"), bg="blue", width=24)
+    lbl_start.grid(row=0)
+
+widgets()   
 
 main.mainloop()
